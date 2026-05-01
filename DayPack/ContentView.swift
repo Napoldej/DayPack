32e95e2ef0bd5ea3a84724b68fd4bee5cee4d237
Napoldej <napoldej.p@ku.th>
2026-05-01 15:38:18 +0700

    Initial Commit

---
+++ DayPack/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DayPack
+//
+//  Created by winpassornratchakul on 1/5/2569 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DayPack/DayPackApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayPackApp.swift
+//  DayPack
+//
+//  Created by winpassornratchakul on 1/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct DayPackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
