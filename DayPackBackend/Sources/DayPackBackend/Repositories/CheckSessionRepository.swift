ea81d0e7c4dc2da686ddd34031b7b5878ae540a2
Sunthorn Kompita <sunthorn.ko@ku.th>
2026-05-11 18:34:34 +0700

    Tighten CheckSessionRepository query scope

--- DayPackBackend/Sources/DayPackBackend/Repositories/CheckSessionRepository.swift
+++ DayPackBackend/Sources/DayPackBackend/Repositories/CheckSessionRepository.swift
@@ -28,7 +28,7 @@ struct CheckSessionRepository: CheckSessionRepositoryProtocol {
 
     func create(_ session: CheckSession, on db: any Database) async throws -> CheckSession {
         try await session.save(on: db)
-        return session
+        return try await findLoaded(id: try session.requireID(), on: db)
     }
 
     func complete(id: UUID, on db: any Database) async throws -> CheckSession {
@@ -37,7 +37,7 @@ struct CheckSessionRepository: CheckSessionRepositoryProtocol {
         }
         session.completedAt = Date()
         try await session.save(on: db)
-        return session
+        return try await findLoaded(id: id, on: db)
     }
 
     func delete(id: UUID, on db: any Database) async throws {
@@ -45,5 +45,12 @@ struct CheckSessionRepository: CheckSessionRepositoryProtocol {
             throw Abort(.notFound)
         }
         try await session.delete(on: db)
+    }
+
+    private func findLoaded(id: UUID, on db: any Database) async throws -> CheckSession {
+        guard let session = try await find(id: id, on: db) else {
+            throw Abort(.notFound)
+        }
+        return session
     }
 }
